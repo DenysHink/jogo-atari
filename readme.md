3d478fceed3e61b70e3d86f8ebf429426f14ebb9
DENNIS HINKEL DA SILVA <dennis_h_silva@estudante.sesisenai.org.br>
2025-04-15 12:03:43 -0300

    jogo espaco

---
+++ readme.md
@@ -0,0 +1,18 @@
+# 👾 Space Shooter - Jogo 2D com Canvas e POO
+
+Este projeto é um jogo 2D inspirado em Space Invaders, desenvolvido como atividade avaliativa de Programação Orientada a Objetos. O jogo utiliza HTML5 Canvas e JavaScript, com foco em estruturação orientada a objetos e versionamento com Git/GitHub.
+
+
+- Você controla uma nave azul que se move para os lados A e D e as SETAS, 
+- Pressionando o espaço você atira, simulando uma metralhadora, assim quando o tiro acerta o alien, destrói ele, quando o alien cai no chão. GAME OVER,
+
+
+
+Tecnologias Utilizadas
+
+- HTML5 + Canvas
+- JavaScript
+- Programação Orientada a Objetos
+- Git & GitHub
+
+
--- script.js
+++ script.js
@@ -1,32 +1,185 @@
-const canvas = document.getElementById('JogoCanvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById('JogoCanvas');
+const ctx = canvas.getContext('2d');
 
+const imagemAlien = new Image();
+imagemAlien.src = 'alien.png';
 
-class Entidade{
-    constructor(x,y,largura,altura,cor){
-        this.x = x,
-        this.y = y,
-        this.largura = largura,
-        this.altura = altura,
-        this.cor = cor
+const imagemJogador = new Image();
+imagemJogador.src = 'foguete.jpg'; 
+
+class Entidade {
+    constructor(x, y, largura, altura, cor) {
+        this.x = x;
+        this.y = y;
+        this.largura = largura;
+        this.altura = altura;
+        this.cor = cor;
     }
-    desenhar(){
-        ctx.fillStyle = this.cor
-        ctx.fillRect(this.x, this.y, this.largura, this.altura)
+
+    desenhar() {
+        ctx.fillStyle = this.cor;
+        ctx.fillRect(this.x, this.y, this.largura, this.altura);
+    }
+
+    colideCom(outro) {
+        return (
+            this.x < outro.x + outro.largura &&
+            this.x + this.largura > outro.x &&
+            this.y < outro.y + outro.altura &&
+            this.y + this.altura > outro.y
+        );
     }
 }
 
-const alien1 = new Entidade(350, 0, 50, 50, 'green')
-const objeto_na_tela = new Entidade(350,350,50,50,'red')
+class Jogador extends Entidade {
+    constructor(x, y) {
+        super(x, y, 50, 30, 'blue');
+        this.velocidade = 5;
+        this.direcao = 0;
+    }
 
+    mover() {
+        this.x += this.velocidade * this.direcao;
+        if (this.x < 0) this.x = 0;
+        if (this.x + this.largura > canvas.width) this.x = canvas.width - this.largura;
+    }
 
+    desenhar() {
+        ctx.drawImage(imagemJogador, this.x, this.y, this.largura, this.altura);
+    }
 
-function loop(){
-    ctx.clearRect(0,0,canvas.width, canvas.height)
-    objeto_na_tela.desenhar()
-    alien1.desenhar()
- 
-    //inserir as funções de desenhar, atualizar, colisão aqui
-    requestAnimationFrame(loop)
+    atualizar() {
+        this.mover();
+        this.desenhar();
+    }
 }
-loop()
+
+class Projétil extends Entidade {
+    constructor(x, y) {
+        super(x, y, 5, 10, 'yellow');
+        this.velocidade = 6;
+    }
+
+    atualizar() {
+        this.y -= this.velocidade;
+        this.desenhar();
+    }
+}
+
+class Alien extends Entidade {
+    constructor(x, y) {
+        super(x, y, 40, 30, 'green');
+        this.velocidade = 2;
+    }
+
+    atualizar() {
+        this.y += this.velocidade;
+        ctx.drawImage(imagemAlien, this.x, this.y, this.largura, this.altura);
+    }
+}
+
+const jogador = new Jogador(375, 350);
+const projeteis = [];
+const aliens = [];
+let pontuacao = 0;
+let gameOver = false;
+
+let atirando = false;
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowLeft' || e.key === 'a') jogador.direcao = -1;
+    if (e.key === 'ArrowRight' || e.key === 'd') jogador.direcao = 1;
+    if ((e.key === ' ' || e.key === 'Spacebar') && !gameOver && !atirando) {
+        atirando = true;
+        dispararTiros();  
+    }
+    if (gameOver && e.key === 'Enter') {
+        reiniciarJogo();
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    if (
+        e.key === 'ArrowLeft' ||
+        e.key === 'ArrowRight' ||
+        e.key === 'a' ||
+        e.key === 'd'
+    ) {
+        jogador.direcao = 0;
+    }
+    if (e.key === ' ' || e.key === 'Spacebar') {
+        atirando = false;
+    }
+});
+
+function dispararTiros() {
+    if (!gameOver && atirando) {
+        projeteis.push(new Projétil(jogador.x + jogador.largura / 2 - 2.5, jogador.y));
+        setTimeout(dispararTiros, 100); 
+    }
+}
+
+function criarAlien() {
+    if (!gameOver) {
+        const x = Math.random() * (canvas.width - 40);
+        aliens.push(new Alien(x, 0));
+    }
+}
+setInterval(criarAlien, 1000);
+
+function reiniciarJogo() {
+    projeteis.length = 0;
+    aliens.length = 0;
+    pontuacao = 0;
+    gameOver = false;
+    jogador.x = 375;
+    loop();
+}
+
+function loop() {
+    if (gameOver) {
+        ctx.fillStyle = 'black';
+        ctx.font = '30px Arial';
+        ctx.fillText('Game Over! Pontuação: ' + pontuacao, 200, 200);
+        ctx.font = '16px Arial';
+        ctx.fillText('Reiniciando em 3 segundos... ou aperte Enter', 230, 240);
+
+        setTimeout(() => {
+            if (gameOver) reiniciarJogo();
+        }, 3000);
+        return;
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    jogador.atualizar();
+
+    for (let i = projeteis.length - 1; i >= 0; i--) {
+        projeteis[i].atualizar();
+        if (projeteis[i].y < 0) projeteis.splice(i, 1);
+    }
+
+    for (let i = aliens.length - 1; i >= 0; i--) {
+        const alien = aliens[i];
+        alien.atualizar();
+
+        if (alien.y + alien.altura >= canvas.height || alien.colideCom(jogador)) {
+            gameOver = true;
+        }
+
+        for (let j = projeteis.length - 1; j >= 0; j--) {
+            if (projeteis[j].colideCom(alien)) {
+                aliens.splice(i, 1);
+                projeteis.splice(j, 1);
+                pontuacao++;
+                break;
+            }
+        }
+    }
+
+    ctx.fillStyle = 'white';
+    ctx.font = '20px Arial';
+    ctx.fillText('Pontuação: ' + pontuacao, 10, 20);
+
+    requestAnimationFrame(loop);
+}
+loop();
